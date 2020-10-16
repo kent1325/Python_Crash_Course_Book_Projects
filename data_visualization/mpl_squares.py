04e30a2d237d06c5e8b7a3e037621239cdb52511
Kent Vugs Nielsen <19304326+kent1325@users.noreply.github.com>
2020-10-16 22:02:30 +0200

    initial commit

---
+++ data_visualization/mpl_squares.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+x_values = range(1, 5001)
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.magma, s=11)
+
+# Set chart title and label axes.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+ax.axis([0, 5100, 0, 150000000000])
+
+plt.show()
